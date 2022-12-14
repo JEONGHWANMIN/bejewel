c45894759377a1ca5c7a6f76a8b712ac39f91037
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-14 23:01:43 +0900

    Feat : 초기 CSS 초기화 및 폰트 설정

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+  *, body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
