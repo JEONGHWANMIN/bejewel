6db0fdacebfde57b9a15fcb38739b3fb57dcf7c7
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-15 22:28:07 +0900

    Remove : Header 컴포넌트 제거

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { GlobalStyle } from "./style/GlobalStyle";
 import { router } from "./pages/router";
-import Header from "./components/Header";
 import "./style/fonts.css";
 
 const root = ReactDOM.createRoot(
@@ -13,7 +12,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Header />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
