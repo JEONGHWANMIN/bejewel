3b0970dfc520b61ec9a5bac1abd2abed5bc286fb
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-15 22:29:10 +0900

    Feat : 라벨 인풋 컴포넌트 추가

---
+++ src/components/Enroll/LabelInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import Input from "./Input";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+function LabelInput({ label, ...props }: Props) {
+  const [focus, setFocus] = useState(false);
+  return (
+    <Block onFocus={() => setFocus(true)} onBlur={() => setFocus(false)}>
+      <Label focus={focus}>{label}</Label>
+      <Input {...props} />
+    </Block>
+  );
+}
+
+export default LabelInput;
+
+const Block = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const Label = styled.label<{ focus?: boolean }>`
+  font-size: 18px;
+  font-weight: 700;
+  padding-left: 10px;
+  color: gray;
+
+  ${({ focus }) =>
+    focus &&
+    css`
+      transition: all 0.3s ease-in-out;
+      color: black;
+    `}
+`;
