fff07bcd4d6ec53150cd4a43dd3d29be5b5fdd95
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-16 21:05:06 +0900

    Feat : 상품 데이터 loader 함수 추가

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
+import { getProducts } from "src/apis/product";
 import Enroll from "./Enroll";
 import Home from "./Home";
 
@@ -6,6 +7,11 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    loader: async () => {
+      const data = await getProducts();
+      if (!data) throw redirect("/404");
+      return data;
+    },
   },
   {
     path: "/enroll",
