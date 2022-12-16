640cbfa08a80009044acdb33cca118ddbc7e8ab3
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-16 21:02:32 +0900

    Feat : 상품 등록 페이지 레이아웃 추가

--- src/pages/Enroll/index.tsx
+++ src/pages/Enroll/index.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import Layout from "src/components/Layout/Layout";
+import styled from "styled-components";
+import ProductForm from "src/components/Enroll/ProductForm";
 
 function Enroll() {
-  return <div>Enroll</div>;
+  return (
+    <Layout>
+      <Container>
+        <Title>상품 등록하기</Title>
+        <ProductForm />
+      </Container>
+    </Layout>
+  );
 }
 
 export default Enroll;
+
+const Container = styled.div`
+  border: solid 1px lightgray;
+  width: 560px;
+  margin: auto;
+  margin-top: 2%;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  margin-bottom: 30px;
+`;
