5b8a9672897ee34d42511a21a9dd3b396ce9a60b
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-16 21:04:00 +0900

    Feat : 상품 데이터 폼 , 데이터 관리 로직 추가

--- src/components/Enroll/ProductForm.tsx
+++ src/components/Enroll/ProductForm.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useForm from "src/hooks/Enroll/useForm";
 import styled from "styled-components";
 import FileUpload from "./FileUpload";
@@ -7,36 +6,38 @@ import LabelInput from "./LabelInput";
 import SubmitButton from "./SubmitButton";
 
 function ProductForm() {
-  const { form, onChange, onSubmit } = useForm();
+  const { form, error, handleChange, handleSubmit, handleFileChange } =
+    useForm();
 
   return (
-    <Form onSubmit={onSubmit}>
-      <FileUpload url={form.url} />
+    <Form onSubmit={handleSubmit}>
+      <FileUpload url={form.url} handleFileChange={handleFileChange} />
       <LabelInput
         name="brand"
         label={"브랜드"}
-        onChange={onChange}
+        onChange={handleChange}
         value={form.brand}
       />
       <LabelInput
         name="name"
         label={"상품명"}
-        onChange={onChange}
+        onChange={handleChange}
         value={form.name}
       />
       <LabelInput
         name="price"
         label={"가격"}
-        onChange={onChange}
+        onChange={handleChange}
         value={form.price}
         placeholder={"2000원"}
       />
       <LabelCheckBox
         name="freeDelivery"
-        onChange={onChange}
+        onChange={handleChange}
         checked={form.freeDelivery}
       />
       <SubmitButton title="상품등록" />
+      {error && <Error>모든 항목을 입력해주세요.</Error>}
     </Form>
   );
 }
@@ -49,4 +50,10 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
   gap: 30px;
+`;
+
+const Error = styled.div`
+  color: red;
+  font-size: 20px;
+  font-weight: 600;
 `;
