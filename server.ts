5f80b79a128cc5a2252879b231553c07ffc18735
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-17 19:43:14 +0900

    Feat : 상품 데이터 페이지 네이션 추가

--- server.ts
+++ server.ts
@@ -1,14 +1,26 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import { db } from "./db";
+import { getProductsDB } from "./db";
 
 const app: Express = express();
 app.use(cors());
 
 const port = 8080;
 
-app.get("/products", (req: Request, res: Response) => {
-  res.json(db);
+app.get("/products", async (req: Request, res: Response) => {
+  const products = await getProductsDB();
+
+  let { page, size } = req.query;
+
+  const totalPage = Math.ceil(products.length / Number(size));
+  const offset = (Number(page) - 1) * Number(size);
+
+  const result = products.slice(offset, offset + Number(size));
+
+  res.json({
+    totalPage,
+    products: result,
+  });
 });
 
 app.post("/product", (req: Request, res: Response) => {
@@ -24,5 +36,5 @@ app.delete("/product", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
