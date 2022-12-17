db4a558e193fbbb769ff713ff39bf8033d626ca1
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-17 23:27:08 +0900

    Feat : 상품 등록 타입 추가

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,6 +3,14 @@ export interface ProductsResponse {
   totalPage: number;
 }
 
+export interface CreateProduct {
+  url: string | ArrayBuffer;
+  brand: string;
+  name: string;
+  price: number;
+  freeDelivery: boolean;
+}
+
 export interface Product {
   id: number;
   url: string | ArrayBuffer;
