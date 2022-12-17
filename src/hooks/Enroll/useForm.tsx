5a60f5d06116cd609848e14403a88366f55f2b64
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-17 23:26:57 +0900

    Feat : handleSubmit 로직 추가

--- src/hooks/Enroll/useForm.tsx
+++ src/hooks/Enroll/useForm.tsx
@@ -1,36 +1,32 @@
-import React, { useState } from "react";
-
-interface Form {
-  url: string | ArrayBuffer;
-  brand: string;
-  name: string;
-  price: string;
-  freeDelivery: boolean;
-}
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createProduct } from 'src/apis/product';
+import { type CreateProduct } from 'src/types/product';
 
 function useForm() {
+  const navigate = useNavigate();
   const [error, setError] = useState(false);
-  const [form, setForm] = useState<Form>({
-    url: "",
-    brand: "",
-    name: "",
-    price: "",
+  const [form, setForm] = useState<CreateProduct>({
+    url: '',
+    brand: '',
+    name: '',
+    price: 0,
     freeDelivery: false,
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value, type, checked } = e.target;
     setForm({
       ...form,
-      [name]: type === "checkbox" ? checked : value,
+      [name]: type === 'checkbox' ? checked : value,
     });
 
     if (error) setError(false);
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.currentTarget.files?.[0];
-    if (!file) return alert("파일이 없습니다.");
+    if (!file) return alert('파일이 없습니다.');
     const fileReader = new FileReader();
     fileReader.onload = (e) => {
       const { result } = e.target as FileReader;
@@ -44,7 +40,7 @@ function useForm() {
     fileReader.readAsDataURL(file);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const { brand, name, price, url } = form;
 
@@ -53,7 +49,12 @@ function useForm() {
       return;
     }
 
-    console.log(form);
+    const result = await createProduct(form);
+
+    if (result.status === 200) {
+      window.alert('상품이 등록되었습니다.');
+      navigate('/');
+    }
   };
 
   return { form, error, handleChange, handleSubmit, handleFileChange };
