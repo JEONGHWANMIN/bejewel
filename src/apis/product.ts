72411173edf1430365639a1003a2d7750732d398
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-17 23:25:25 +0900

    Feat : 상품 등록 API 추가

--- src/apis/product.ts
+++ src/apis/product.ts
@@ -1,14 +1,25 @@
-import axios from "axios";
-import { type ProductsResponse } from "src/types/product";
+import axios from 'axios';
+import { type CreateProduct, type ProductsResponse } from 'src/types/product';
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: 'http://localhost:8080',
   timeout: 5000,
 });
 
 export async function getProducts(page: number) {
-  const result = await instance.get<ProductsResponse>(
-    `/products?size=6&page=${page}`
-  );
-  return result.data;
+  try {
+    const result = await instance.get<ProductsResponse>(`/products?size=6&page=${page}`);
+    return result.data;
+  } catch (e) {
+    throw new Error('전체 상품데이터 조회 실패');
+  }
+}
+
+export async function createProduct(product: CreateProduct) {
+  try {
+    const result = await instance.post('/product', product);
+    return result;
+  } catch (e) {
+    throw new Error('상품 등록 실패');
+  }
 }
