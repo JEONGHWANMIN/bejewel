5c2eb4cbd0c0c6e3edc5af4dff670aaf8707d225
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-17 23:26:21 +0900

    Refactor : printWidth 변경

--- src/components/Enroll/ProductForm.tsx
+++ src/components/Enroll/ProductForm.tsx
@@ -1,41 +1,27 @@
-import useForm from "src/hooks/Enroll/useForm";
-import styled from "styled-components";
-import FileUpload from "./FileUpload";
-import LabelCheckBox from "./LabelCheckBox";
-import LabelInput from "./LabelInput";
-import SubmitButton from "./SubmitButton";
+import useForm from 'src/hooks/Enroll/useForm';
+import styled from 'styled-components';
+import FileUpload from './FileUpload';
+import LabelCheckBox from './LabelCheckBox';
+import LabelInput from './LabelInput';
+import SubmitButton from './SubmitButton';
 
 function ProductForm() {
-  const { form, error, handleChange, handleSubmit, handleFileChange } =
-    useForm();
+  const { form, error, handleChange, handleSubmit, handleFileChange } = useForm();
 
   return (
     <Form onSubmit={handleSubmit}>
       <FileUpload url={form.url} handleFileChange={handleFileChange} />
+      <LabelInput name="brand" label={'브랜드'} onChange={handleChange} value={form.brand} />
+      <LabelInput name="name" label={'상품명'} onChange={handleChange} value={form.name} />
       <LabelInput
-        name="brand"
-        label={"브랜드"}
-        onChange={handleChange}
-        value={form.brand}
-      />
-      <LabelInput
-        name="name"
-        label={"상품명"}
-        onChange={handleChange}
-        value={form.name}
-      />
-      <LabelInput
+        type={'number'}
         name="price"
-        label={"가격"}
+        label={'가격'}
         onChange={handleChange}
         value={form.price}
-        placeholder={"2000원"}
+        placeholder={'숫자만 입력해 주세요. 예) 10000'}
       />
-      <LabelCheckBox
-        name="freeDelivery"
-        onChange={handleChange}
-        checked={form.freeDelivery}
-      />
+      <LabelCheckBox name="freeDelivery" onChange={handleChange} checked={form.freeDelivery} />
       <SubmitButton title="상품등록" />
       {error && <Error>모든 항목을 입력해주세요.</Error>}
     </Form>
