91286e66637ccf51cec804a6f4c1f0854ac85e7e
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-18 20:08:47 +0900

    Feat : 상품 수정 페이지 추가

---
+++ src/pages/Edit/index.tsx
@@ -0,0 +1,35 @@
+import { useParams } from 'react-router-dom';
+import ProductForm from 'src/components/Enroll/ProductForm';
+import Layout from 'src/components/Layout/Layout';
+import useProductFetch from 'src/hooks/Fetch/useProductFetch';
+import styled from 'styled-components';
+
+function Edit() {
+  const { id } = useParams();
+  const { product } = useProductFetch(id!);
+  return (
+    <Layout>
+      <Container>
+        <Title>상품 등록하기</Title>
+        <ProductForm product={product} />
+      </Container>
+    </Layout>
+  );
+}
+
+export default Edit;
+
+const Container = styled.div`
+  border: solid 1px lightgray;
+  width: 560px;
+  margin: auto;
+  margin-top: 2%;
+  padding: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  margin-bottom: 30px;
+`;
