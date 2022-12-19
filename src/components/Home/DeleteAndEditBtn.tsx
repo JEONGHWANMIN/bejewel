1343cf55fe6441c7986631229f1cc826186f7efa
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-19 22:05:33 +0900

    Feat : 상품 삭제 및 수정 기능 추가

--- src/components/Home/DeleteAndEditBtn.tsx
+++ src/components/Home/DeleteAndEditBtn.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { deleteProduct } from 'src/apis/product';
 import styled from 'styled-components';
 
 interface Props {
@@ -10,8 +10,19 @@ function DeleteAndEditBtn({ productId }: Props) {
   const navigate = useNavigate();
   return (
     <Block>
-      <span onClick={() => navigate(`/edit/${productId}`)}>수정</span>
-      <span>삭제</span>
+      <EditButton onClick={() => navigate(`/edit/${productId}`)}>수정</EditButton>
+      <DeleteButton
+        onClick={async () => {
+          if (!window.confirm('정말 삭제하시겠습니까?')) return;
+          const result = await deleteProduct(productId);
+          if (result.status === 200) {
+            alert('삭제되었습니다.');
+            navigate('/');
+          }
+        }}
+      >
+        삭제
+      </DeleteButton>
     </Block>
   );
 }
@@ -21,11 +32,21 @@ export default DeleteAndEditBtn;
 const Block = styled.div`
   display: flex;
   gap: 5px;
+`;
 
-  span {
-    font-size: 14px;
-    font-weight: 300;
-    color: #353434;
-    cursor: pointer;
-  }
+const EditButton = styled.span`
+  font-size: 14px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  background-color: green;
+  color: white;
+  cursor: pointer;
+`;
+const DeleteButton = styled.span`
+  font-size: 14px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  background-color: red;
+  color: white;
+  cursor: pointer;
 `;
