a69316ae948fbff101ea5139843ec2f32ae812f7
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-19 22:04:27 +0900

    Feat : config 설정 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { GlobalStyle } from "./style/GlobalStyle";
-import { router } from "./pages/router";
-import "./style/fonts.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { GlobalStyle } from './style/GlobalStyle';
+import { router } from './pages/router';
+import './style/fonts.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
