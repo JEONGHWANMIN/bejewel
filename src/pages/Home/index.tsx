0932012084aad2b03b72d82d1cfb09d514956b29
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-19 22:33:19 +0900

    Feat : 페이지 타이틀 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
-import Layout from "src/components/Layout/Layout";
-import styled from "styled-components";
-import ProductList from "src/components/Home/ProductList";
+import Layout from 'src/components/Layout/Layout';
+import styled from 'styled-components';
+import ProductList from 'src/components/Home/ProductList';
 
 function Home() {
   return (
     <Layout>
       <Container>
+        <Title>상품 목록</Title>
         <ProductList />
       </Container>
     </Layout>
@@ -17,4 +18,11 @@ export default Home;
 const Container = styled.div`
   max-width: 1200px;
   margin: auto;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  margin: 30px;
 `;
