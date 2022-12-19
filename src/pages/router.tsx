36611b67ad4ab151bcc9c01ab2730746f9253cbd
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-19 22:06:35 +0900

    Feat : 상품 디테일 페이지 추가

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Detail from './Detail';
 import Edit from './Edit';
 import Enroll from './Enroll';
 import Home from './Home';
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: '/enroll',
     element: <Enroll />,
+  },
+  {
+    path: '/detail/:id',
+    element: <Detail />,
   },
   {
     path: '/edit/:id',
