3597178ce594401abefd606fc03eb3effd7a137d
JEONGHWANMIN <mimi0j0j@naver.com>
2022-12-19 22:06:45 +0900

    Feat : 상품 디테일 페이지 레이아웃 추가

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,71 @@
+import Layout from 'src/components/Layout/Layout';
+import { useParams } from 'react-router';
+import useProductFetch from 'src/hooks/Fetch/useProductFetch';
+import { currencyPrice } from 'src/utils/utill';
+import styled from 'styled-components';
+import DeleteAndEditBtn from 'src/components/Home/DeleteAndEditBtn';
+import FreeShipTag from 'src/components/Home/FreeShipTag';
+
+function Detail() {
+  const { id } = useParams();
+  const { product, isLoading } = useProductFetch(id!);
+
+  if (isLoading || !product) return <div>로딩중</div>;
+
+  return (
+    <Layout>
+      <Container>
+        <LeftBox>
+          <img src={String(product.url)} alt="상품 이미지" />
+        </LeftBox>
+        <RightBox>
+          {product.freeDelivery && <FreeShipTag />}
+          <Brand>{product.brand}</Brand>
+          <Name>{product.name}</Name>
+          <Price>{currencyPrice(product.price)}원</Price>
+          <DeleteAndEditBtn productId={product.id} />
+        </RightBox>
+      </Container>
+    </Layout>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 1000px;
+  height: 100%;
+  margin: auto;
+  gap: 20px;
+  margin-top: 10px;
+`;
+
+const LeftBox = styled.div`
+  width: 50%;
+
+  img {
+    width: 100%;
+    height: 400px;
+  }
+`;
+const RightBox = styled.div`
+  width: 50%;
+`;
+
+const Brand = styled.p`
+  font-size: 24px;
+  font-weight: 900;
+  margin: 8px 0;
+`;
+const Name = styled.p`
+  font-size: 24px;
+  color: #353434;
+  margin-bottom: 15px;
+`;
+const Price = styled.p`
+  font-size: 24px;
+  color: #353434;
+  margin-bottom: 15px;
+`;
